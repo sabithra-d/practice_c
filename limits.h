6241df083bee1ce9c1474e7bf4cf6fd923d797da
Sabithra.D <72975256+sabithra-d@users.noreply.github.com>
2021-05-14 16:57:42 +0530

    Create limits.h

---
+++ limits.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<limits.h>
+
+int main()
+{
+    printf("signed short min value:%d\n",SHRT_MIN);
+    printf("signed short max value:%d\n",SHRT_MAX);
+    printf("unsigned short max value:%d\n",USHRT_MAX);
+    return 0;
+    
+}
+//op
+-32768
+  32767
+  65535
